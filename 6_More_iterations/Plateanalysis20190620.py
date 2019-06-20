40c484014de571fa4e5feb01a4ab00afb6963a30
jamesengleback <36072797+jamesengleback@users.noreply.github.com>
2019-06-20 14:30:27 +0100

    Add files via upload

---
+++ 6_More_iterations/Plateanalysis20190620.py
@@ -0,0 +1,230 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import random
+import os
+import itertools
+from scipy import optimize
+
+
+
+def getPlateData(path):
+    '''
+    This reads full range UV-Vis trace from a BMG PheraStar Platereader
+    as a csv and does a bit or data munging and returns a pandas dataframe
+    and the details of the experiment that the machine records, which I'm
+    calling 'metadata'
+    '''
+    data = pd.read_csv(path, skiprows = 6)
+    metadata = pd.read_csv(path, nrows = 3)
+
+    # rename some columns for readability
+    #.columns[0:3] = ['WellLetter','WellNumber', 'SampleID']
+    data.rename(columns = {'Unnamed: 0':'WellLetter','Unnamed: 1':'WellNumber','Unnamed: 2':'SampleID',}, inplace = True)
+    data.dropna(inplace = True, how = 'all')
+    unused_wells = data['SampleID'].str.contains('unused')
+    data = data.loc[unused_wells == False] ### The bool statement flips and strips unused wells
+    WellIndex = data.loc[:,'WellLetter'].str.cat(data.loc[:,'WellNumber'].astype(str))
+    data.index = WellIndex
+    data = data.loc[:,'220':].dropna(axis = 1) # Numerical data only!
+    data.columns = data.columns.astype(int)
+    # zero at 800 nm
+    data.reset_index(drop=True,inplace=True) # otherwise SUBTRACT can't match up cells
+    data = data.subtract(data.loc[:,800],axis=0)
+    data.index = WellIndex
+    return data
+
+def baseline_correction(data):
+    data.reset_index(inplace=True)
+    baseline = data[data['WellLetter'].str.contains('4')]
+    baseline = baseline.mean(axis=0)
+    # takes mean
+    data = data.drop('WellLetter',axis=1)
+    data = data.subtract(baseline, axis=1)
+
+    return data
+
+def WhosTheWorstOffender(data):
+    data = pd.DataFrame(data.loc[:,320])
+    data.reset_index(inplace=True)
+    data.sort_values([320],inplace=True,ascending=False)
+    return data
+
+def select_traces(data, selection):
+    # assumes that the data has a well number index
+    data.reset_index(inplace=True)
+    well_index = data['WellLetter']
+    data = data.loc[well_index.str.contains(str(selection)),:]
+    data.drop(['WellLetter'],inplace=True, axis=1)
+    return data
+
+def plotPlateData(data, title):
+    ## = pd.Series([list(i)[1] for i in Data_Frames[0].reset_index()['WellLetter']]).astype(int)
+    x = data.columns.astype(int)
+    fig, ax = plt.subplots(figsize=(5,5))
+    #plt.figure(figsize=(15,5))
+    colors = {1:'#304360',
+    2:'#3f6c77',
+    3:'#41a48c'}
+    ax.set_prop_cycle('color',plt.cm.inferno(np.linspace(0,0.9,len(data))))
+    plt.set_cmap('viridis')
+
+    concs = np.array([1,1,
+    0.5,0.5,
+    0.25,0.25,
+    0.125,0.125,
+    0.0625,0.0625,
+    0.03125,0.03125,
+    0.015625,0.015625,
+    0.0078125,0.0078125])*1000
+
+    concs = np.around(concs,2)
+    for i in range(len(data)):
+        y = data.iloc[i,:]
+        plt.plot(x,y, lw = 2, alpha = 0.7)
+    plt.title(title + ' Corrected Spectra')
+    plt.xticks(x[::50])
+    plt.xlim((220,800))
+    plt.ylim((-0.05,0.2))
+    plt.xlabel('Wavlength nm')
+    plt.ylabel('Absorbance')
+    plt.legend(concs, title = 'Substrate concentration in uM')
+    plt.show()
+    #plt.savefig((title + ' Corrected Spectra PM.png').replace(' ','_'))
+
+def plotPlateDifferenceSpectra(data,pureprotein,title):
+    data.reset_index(drop=True,inplace=True)
+    pureprotein.reset_index(drop=True,inplace=True)
+    data = data.subtract(pureprotein,axis=1)
+    x = data.columns.astype(int)
+    fig, ax = plt.subplots(figsize=(7,2.5))
+    ax.set_prop_cycle('color',plt.cm.inferno(np.linspace(0,0.9,len(data))))
+    plt.set_cmap('viridis')
+    concs = np.array([1,1,
+    0.5,0.5,
+    0.25,0.25,
+    0.125,0.125,
+    0.0625,0.0625,
+    0.03125,0.03125,
+    0.015625,0.015625,
+    0.0078125,0.0078125])*1000
+
+    concs = np.around(concs,2)
+    for i in range(len(data)):
+        y = data.iloc[i,:]
+        plt.plot(x,y, lw = 2, alpha = 0.7)
+    plt.title(title + ' Difference Spectra')
+    plt.xticks(x[::50])
+    plt.xlim((350,800))
+    plt.ylim((-0.1,0.1))
+    plt.xlabel('Wavlength nm')
+    plt.ylabel('Change in Absorbance')
+    plt.legend(concs, title = 'Substrate concentration in uM',loc='right')
+    plt.show()
+    #plt.savefig((title + ' Difference Spectra PM.png').replace(' ','_'))
+
+def calculateDiffDiff(data,pureprotein):
+    data=data.subtract(pureprotein,axis=1)
+    DiffA420=data.loc[:,420]
+    DiffA390=data.loc[:,390]
+    DiffDiff = DiffA390-DiffA420
+    return DiffDiff
+
+def curve(x, vmax, km):
+    y = (vmax*x)/(km + x)
+    return y
+
+def calculate_Km(DiffDiff,concs):
+    params, cov = optimize.curve_fit(curve, concs, DiffDiff,
+                                                   p0=[2, 2])
+    vmax = params[0]
+    km = params[1]
+    return vmax, km
+
+def Plot_MichaelisMenten(DiffDiff,concs, title):
+    vmax, km = calculate_Km(DiffDiff,concs)
+    x2 = np.linspace(0,concs.max(), 500)
+    y2 = curve(x2, vmax, km)
+    # R^2
+    residuals = DiffDiff - curve(concs, vmax, km)
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((DiffDiff-np.mean(DiffDiff))**2)
+    r_squared = 1 - (ss_res / ss_tot)
+
+    pos1 = y2.max()#-y2.min()
+    pos2 = pos1 - 7*(y2.max()-y2.min())/8
+    fig, ax = plt.subplots(figsize=(7.5,5))
+    plt.set_cmap('inferno')
+    plt.plot(x2, y2,color = '0.1')
+    plt.scatter(concs, DiffDiff,  color = 'orange', s = 30)
+    plt.title(title + 'Michaelis Menten Plot')
+    plt.ylabel('Difference in Abs')
+    plt.xlabel('[Substrate] µM')
+    plt.text(800,y2.max()/2,'Km = '+str(np.around(km,2))+'\n'\
+    +'Vmax = '+str(np.around(vmax,2))+'\n'\
+    +'R squared = '+str(np.around(r_squared,2)))
+
+    #plt.show()
+    plt.savefig((title + ' Michaelis Menten PM.png').replace(' ','_'))
+
+
+def subtract_evryOtherRow(data):
+    rowswithprotein = ['A', 'C', 'E', 'G', 'I', 'K', 'M', 'O']
+    well_index = pd.Series(data.index).str.extract(r'(\w)',expand=False)
+    rowswithprotein = well_index.loc[well_index.isin(rowswithprotein)]
+
+    dataWithProtein = data.iloc[rowswithprotein.index,:]
+    datawithoutprotein = data.drop(dataWithProtein.index)
+    indexExceptForRealThisTime = dataWithProtein.index
+    dataWithProtein.reset_index(drop=True,inplace=True)
+    datawithoutprotein.reset_index(drop=True,inplace=True)
+    output = dataWithProtein - datawithoutprotein
+    output.index=indexExceptForRealThisTime
+    return output
+
+def StickItAllTogether(path, selection,concs,title):
+    '''selection should match the well column number'''
+    data = getPlateData(path)
+    well_index = data.index
+    data = baseline_correction(data)
+    data.index = well_index
+    data=subtract_evryOtherRow(data)
+    '''
+    Little bit of selecting the right columns
+    '''
+    well_columns = pd.Series(data.index).str.extract(r'(\d+)').astype(int)
+    pureprotein_andDMSO = data.iloc[well_columns[well_columns==13].dropna().index]
+    well_columns=well_columns[well_columns==selection].dropna().index
+    data=data.iloc[well_columns]
+    diffdiff=calculateDiffDiff(data.reset_index(drop=True),pureprotein_andDMSO.reset_index(drop=True))
+    #plotPlateData(data,title)
+    #plotPlateDifferenceSpectra(data,pureprotein_andDMSO,title)
+    Plot_MichaelisMenten(diffdiff, concs,title)
+
+
+concs = np.array([1,
+0.5,
+0.25,
+0.125,
+0.0625,
+0.03125,
+0.015625,
+0.0078125])*1000
+
+titles = ['protein and dmso',
+'arachadnic acid 1.1','arachadnic acid 1.2',
+'arachadnic acid 2.1','arachadnic acid 2.2',
+'arachadnic acid 3.1','arachadnic acid 3.2',
+'Palmitic acid 1.1','Palmitic acid 1.2',
+'Palmitic acid 2.1','Palmitic acid 2.2',
+'Palmitic acid 3.1','Palmitic acid 3.2',
+'Lauric acid 1.1','Lauric acid 1.2',
+'Lauric acid 2.1','Lauric acid 2.2',
+'Lauric acid 3.1','Lauric acid 3.2'
+]
+
+count=0
+for i in range(1,20):
+    StickItAllTogether('20190620_AM.CSV',i,concs, titles[count])
+    count+=1
---
+++ 6_More_iterations/readme.md
@@ -0,0 +1,78 @@
+### Iteration round 6
+##### Background
+In the last tests I did 9 repeats of my substrates arachadonic acid and 4-phenylimidazole. It was  repeats in the mother plate to test for mixing variability and stuff like that. I got really consistent results with arachadonic acid, which was great, but terrible results with 4-phenylimidazole.
+
+I haven't got to the bottom of that yet, but today I'm going to work on fatty acids and try to get a few of them working with a few repeats of each. I have a few socks of each, so I may as well use all of them to see which is good and which is donked.
+
+Substrates:
+* Arachadonic acid
+* Lauric acid
+* Palmitic acid
+
+That should do for now. I can do 3 reps of each in the mother plate which will serve 6 reps per substrate in the assay plate total.
+
+Here's my layout:
+![](/home/james/Documents/Work/201906_PlateAssayDevelopment/6_More_iterationns/20190620_1_Platelayout.png)
+Same as before, light squares are protein-free controls. Each double column rep is a sing column from the mother plate. I would do three but I can't.
+
+P450 BM3 WT Heme concentration:
+* [Data](20190620_BM3conccheck.csv) from a Cary UV-Vis Spectrometer
+* [Script](ProtinConcCheck.py) to calculate protein concentration
+```
+$ python3 ProtinConcCheck.py
+0   -0.006898
+1    3.869643
+Name: P450 conc/uM, dtype: float64
+```
+* conc = 3.86 uM
+
+It felt like I didn't mess up the pipetting. Used a lot of tips though, so to try to limit that, I pipetted out the compounds first, changing tips between mother plate reps. Added buffer to the blank wells without mixing so that I could use a single row of tips. Same for the protein, being really careful to not let the tips touch the inside of the wells. Then to mix, I pipetted up and down from the low concentrations upward for the protein wells and the buffer wells (seperately) under the assumption that any substrate carried to the next concentration up shouldn't have a big effect. I hope. It saved a lot of tips but Ill see what I can do to make it more efficient.
+
+Carried the whole affair downstairs in my homemade plate carrier
+![](assets/readme-c8e5a655.jpg)
+
+* Plate type: Corning 3660 (check)
+
+And then span at 4000 rpm for 2 mins. Not sure how much is enough, should find that out.
+Scanned the wells for wavelengths 220-800 nm on the Pherastar FS. It took about 3 mins, plus some extra time to transfer the data to the dedicated PC, and then to Open it. I wonder what file type it transfers as, because as an ASCII csv file it opens and transfers really quick. That's one to ask BMG if I ever get the chance.
+
+* Platereader [data](assets/readme-00111b7f.CSV)
+* This old [script](Plateanalysis20190620.py) agian!
+
+*Arachadonic Acid* Looks good again
+
+|Corrected Specs| Difference Specs| Michaelis Menten Curves|
+|---------|---------|---------|
+|![](arachadnic_acid_1.1_Corrected_Spectra_PM.png)|![](arachadnic_acid_1.1_Difference_Spectra_PM.png) | ![](arachadnic_acid_1.1_Michaelis_Menten_PM.png)|
+|![](arachadnic_acid_1.2_Corrected_Spectra_PM.png) | ![](arachadnic_acid_1.2_Difference_Spectra_PM.png) | ![](arachadnic_acid_1.2_Michaelis_Menten_PM.png)|
+|![](arachadnic_acid_2.1_Corrected_Spectra_PM.png)|![]( arachadnic_acid_2.1_Difference_Spectra_PM.png)| ![](arachadnic_acid_2.1_Michaelis_Menten_PM.png)|
+|![](arachadnic_acid_2.2_Corrected_Spectra_PM.png)|![]( arachadnic_acid_2.2_Difference_Spectra_PM.png)| ![](arachadnic_acid_2.2_Michaelis_Menten_PM.png)|
+|![](arachadnic_acid_3.1_Corrected_Spectra_PM.png)|![]( arachadnic_acid_3.1_Difference_Spectra_PM.png)| ![](arachadnic_acid_3.1_Michaelis_Menten_PM.png)|
+|![](arachadnic_acid_3.2_Corrected_Spectra_PM.png)|![]( arachadnic_acid_3.2_Difference_Spectra_PM.png)| ![](arachadnic_acid_3.2_Michaelis_Menten_PM.png)|
+
+**Palmitic acid** sucks
+
+|Corrected Specs| Difference Specs| Michaelis Menten Curves|
+|---------|---------|---------|
+|![](Palmitic_acid_1.1_Corrected_Spectra_PM.png)|![](Palmitic_acid_1.1_Difference_Spectra_PM.png) | ![](Palmitic_acid_1.1_Michaelis_Menten_PM.png)|
+|![](Palmitic_acid_1.2_Corrected_Spectra_PM.png) | ![](Palmitic_acid_1.2_Difference_Spectra_PM.png) | ![](Palmitic_acid_1.2_Michaelis_Menten_PM.png)|
+|![](Palmitic_acid_2.1_Corrected_Spectra_PM.png)|![]( Palmitic_acid_2.1_Difference_Spectra_PM.png)| ![](Palmitic_acid_2.1_Michaelis_Menten_PM.png)|
+|![](Palmitic_acid_2.2_Corrected_Spectra_PM.png)|![]( Palmitic_acid_2.2_Difference_Spectra_PM.png)| ![](Palmitic_acid_2.2_Michaelis_Menten_PM.png)|
+|![](Palmitic_acid_3.1_Corrected_Spectra_PM.png)|![]( Palmitic_acid_3.1_Difference_Spectra_PM.png)| ![](Palmitic_acid_3.1_Michaelis_Menten_PM.png)|
+|![](Palmitic_acid_3.2_Corrected_Spectra_PM.png)|![]( Palmitic_acid_3.2_Difference_Spectra_PM.png)| ![](Palmitic_acid_3.2_Michaelis_Menten_PM.png)|
+
+**Lauric Acid** sucks too.
+
+|Corrected Specs| Difference Specs| Michaelis Menten Curves|
+|---------|---------|---------|
+|![](Lauric_acid_1.1_Corrected_Spectra_PM.png)|![](Lauric_acid_1.1_Difference_Spectra_PM.png) | ![](Lauric_acid_1.1_Michaelis_Menten_PM.png)|
+|![](Lauric_acid_1.2_Corrected_Spectra_PM.png) | ![](Lauric_acid_1.2_Difference_Spectra_PM.png) | ![](Lauric_acid_1.2_Michaelis_Menten_PM.png)|
+|![](Lauric_acid_2.1_Corrected_Spectra_PM.png)|![]( Lauric_acid_2.1_Difference_Spectra_PM.png)| ![](Lauric_acid_2.1_Michaelis_Menten_PM.png)|
+|![](Lauric_acid_2.2_Corrected_Spectra_PM.png)|![]( Lauric_acid_2.2_Difference_Spectra_PM.png)| ![](Lauric_acid_2.2_Michaelis_Menten_PM.png)|
+|![](Lauric_acid_3.1_Corrected_Spectra_PM.png)|![]( Lauric_acid_3.1_Difference_Spectra_PM.png)| ![](Lauric_acid_3.1_Michaelis_Menten_PM.png)|
+|![](Lauric_acid_3.2_Corrected_Spectra_PM.png)|![]( Lauric_acid_3.2_Difference_Spectra_PM.png)| ![](Lauric_acid_3.2_Michaelis_Menten_PM.png)|
+
+It looks like Lauric and Palmitic acids aren't introducing a shift at all. Here are some potential reasons:
+1. These things aren't even substrates anyway (easy to check in literature)
+2. These things are precipitating
+3. I made up my stocks wrong
